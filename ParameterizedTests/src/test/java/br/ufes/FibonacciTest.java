85da6c99949c6850e4ee6aafdc8937a18d048b06
Pedro Henrique Robadel <pedro.camara@itixit.com.br>
2021-08-10 22:43:42 -0300

    Adicionando o projeto ParameterizedTests

---
+++ ParameterizedTests/src/test/java/br/ufes/FibonacciTest.java
@@ -0,0 +1,37 @@
+package br.ufes;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
+
+
+@RunWith(Parameterized.class)
+public class FibonacciTest {
+    @Parameters(name = "{index}: fib({0})={1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {     
+                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }  
+           });
+    }
+    
+    @Parameter
+    public int fInput;
+    
+    @Parameter(1)
+    public int fExpected;
+
+    public FibonacciTest(int input, int expected) {
+        this.fInput = input;
+        this.fExpected = expected;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(fExpected, Fibonacci.compute(fInput));
+    }
+}
