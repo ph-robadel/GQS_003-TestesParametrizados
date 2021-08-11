4fcf0a79da1029d6260e1416cf72657043f8e018
Pedro Henrique Robadel <pedro.camara@itixit.com.br>
2021-08-11 07:50:01 -0300

    Corrigindo teste

--- ParameterizedTests/src/test/java/br/ufes/FibonacciTest.java
+++ ParameterizedTests/src/test/java/br/ufes/FibonacciTest.java
@@ -1,37 +1,34 @@
 package br.ufes;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
-import java.util.Collection;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import org.junit.runners.Parameterized.Parameter;
-
 
 @RunWith(Parameterized.class)
 public class FibonacciTest {
+
     @Parameters(name = "{index}: fib({0})={1}")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {     
-                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }  
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][] { 
+                 { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 }
            });
     }
-    
-    @Parameter
-    public int fInput;
-    
-    @Parameter(1)
-    public int fExpected;
+
+    private int input;
+    private int expected;
 
     public FibonacciTest(int input, int expected) {
-        this.fInput = input;
-        this.fExpected = expected;
+        this.input = input;
+        this.expected = expected;
     }
 
     @Test
     public void test() {
-        assertEquals(fExpected, Fibonacci.compute(fInput));
+        assertEquals(expected, Fibonacci.compute(input));
     }
 }
